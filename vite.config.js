39aa3d81e95a16108e8e5e6f96dc0b30df41e23c
Alexis Lasselin <alexis.lasselin@algosup.com>
2025-04-02 13:31:59 +0200

    jkp

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/2024-2025-project-4-web-fpga-team-3',  // Assure que les chemins sont relatifs
-})
+  base: "", // Assure que les chemins sont relatifs
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
