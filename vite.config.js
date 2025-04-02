436a5fac8f6cd87aa3cb1c7f3e34900987eb2b25
Alexis Lasselin <114481578+AlexisLasselin@users.noreply.github.com>
2025-04-02 13:06:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Assure que les chemins sont relatifs
+  base: '2024-2025-project-4-web-fpga-team-3',  // Assure que les chemins sont relatifs
 })
